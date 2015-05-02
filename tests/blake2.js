d48d8c123fd0eaa3f2e6bd1dd0f7a37a2f194cc5
Ivan Kozik <ivan@ludios.org>
2015-05-02 19:26:30 +0000

    Add test for digest('binary')

--- tests/blake2.js
+++ tests/blake2.js
@@ -5,6 +5,7 @@ const assert = require('assert');
 
 const BLAKE2B_EMPTY_DIGEST_HEX = '786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce';
 const BLAKE2B_EMPTY_DIGEST_BASE64 = new Buffer(BLAKE2B_EMPTY_DIGEST_HEX, 'hex').toString('base64');
+const BLAKE2B_EMPTY_DIGEST_BINARY = new Buffer(BLAKE2B_EMPTY_DIGEST_HEX, 'hex').toString('binary');
 
 describe('BLAKE2bHash', function() {
 	it('should return correct digest after 0 updates', function() {
@@ -20,6 +21,11 @@ describe('BLAKE2bHash', function() {
 	it('should return a base64 string when digest("base64") is called', function() {
 		let hash = blake2.BLAKE2bHash();
 		assert.equal(hash.digest('base64'), BLAKE2B_EMPTY_DIGEST_BASE64);
+	});
+
+	it('should return a binary string when digest("binary") is called', function() {
+		let hash = blake2.BLAKE2bHash();
+		assert.equal(hash.digest('binary'), BLAKE2B_EMPTY_DIGEST_BINARY);
 	});
 
 	it('throws Error if digest() is called a second or third time', function() {
