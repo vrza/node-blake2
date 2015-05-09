4e1694900dc85c04dd12936cbc862ffcbc6b56a1
Ivan Kozik <ivan@ludios.org>
2015-05-09 18:22:30 +0000

    Add b2sum.js example

---
+++ b2sum.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env iojs
+
+/**
+ * Takes an algorithm name and prints the hex digest of stdin.
+ * Example:
+ * cat file | ./b2sum.js blake2b
+ */
+
+const blake2 = require('./index');
+
+const algo = process.argv[2];
+const hash = new blake2.Hash(algo);
+hash.setEncoding('hex');
+
+const stream = process.stdin;
+
+stream.on('end', function() {
+        hash.end();
+        const digest = hash.read();
+        console.log(digest);
+});
+
+stream.pipe(hash);
