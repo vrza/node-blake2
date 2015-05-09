9a3dfecf2028478b7f49cd733c57c2e06babe8f4
Ivan Kozik <ivan@ludios.org>
2015-05-09 20:51:00 +0000

    Add leaktest.js

---
+++ leaktest.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env iojs
+
+"use strict";
+
+/**
+ * Creates Hashes and Hmacs in an infinite loop; use `ps u | grep leaktest`
+ * to verify lack of memory leaks
+ */
+
+const blake2 = require('./index');
+
+function randomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const algos = ['blake2b', 'blake2s', 'blake2bp', 'blake2sp'];
+const encodings = [undefined, 'hex', 'base64', 'binary'];
+let input = new Buffer(String(Math.random()));
+while(1) {
+	let hash = new blake2.Hash(randomElement(algos));
+	hash.update(input);
+	input = hash.digest(randomElement(encodings));
+	if(typeof input == 'string') {
+		input = new Buffer(input.substr(0, 16));
+	}
+
+	let hmac = new blake2.Hmac(randomElement(algos), new Buffer(String(Math.random())));
+	hmac.update(input);
+	input = hmac.digest(randomElement(encodings));
+	if(typeof input == 'string') {
+		input = new Buffer(input.substr(0, 16));
+	}
+}
