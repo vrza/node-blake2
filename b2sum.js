67a4522f56859565f6d6624ffdd4f0f0587fd178
Vladimir Vrzić <vvrzic@gmail.com>
2023-02-24 18:10:13 +0100

    Added check for correct command line arguments

--- b2sum.js
+++ b2sum.js
@@ -3,23 +3,28 @@
 /**
  * Takes an algorithm name and prints the hex digest of stdin.
  * Example:
- * cat file | ./b2sum.js blake2b
+ * $ node b2sum.js blake2b < file
  */
 
 "use strict";
 
-const blake2 = require('./index');
+if (process.argv.length === 2) {
+	const EXIT_USAGE = 1;
+	const progname = process.argv[0].concat(' ', process.argv[1]);
+	const usageMsg = `Usage: ${progname} <algo>`;
+	console.error(usageMsg);
+	process.exit(EXIT_USAGE);
+}
 
 const algo = process.argv[2];
+const blake2 = require('./index');
 const hash = new blake2.Hash(algo);
 hash.setEncoding('hex');
 
 const stream = process.stdin;
-
 stream.on('end', function() {
 	hash.end();
 	const digest = hash.read();
 	console.log(digest);
 });
-
 stream.pipe(hash);
